16eadab3ea8f7c32ee6fbdfcd14b5bffbc9907d3
SpeedSlicer <mathewshaun25@gmail.com>
2026-08-07 16:56:54 -0400

    herz

--- src/test/testv/build.gradle.kts
+++ src/test/testv/build.gradle.kts
@@ -6,12 +6,16 @@ group = "net.ada"
 version = "1.0-SNAPSHOT"
 
 sourceSets {
+
     main {
         java.srcDir("src/common/java")
-        java.srcDir("src/mixins/java")
     }
 }
 
 repositories {
     mavenCentral()
+}
+dependencies {
+    "compileOnly"("net.lenni0451.classtransform:core:1.15.1")
+    "compileOnly"(project(":src:test:testv:test-inner"))
 }
